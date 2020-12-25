07d0431e208c220ddb9a895637260caf2a4e6854
Ali Davoodi <aa.davoodi.b@gmail.com>
2020-12-25 21:09:18 +0330

    Remove the excess

--- Python/ball_tracker.py
+++ Python/ball_tracker.py
@@ -61,8 +61,7 @@ def init_serial():
 
 
 init_serial()
-updateServos(servoY,servoX)
-updateServos(servoY,servoX)
+#command servo motors to initial position
 updateServos(servoY,servoX)
 
 # construct the argument parse and parse the arguments
@@ -157,72 +156,31 @@ while True:
             #Save The Data Points
             Data_Points.loc[Data_Points.size/3] = [x , y, current_time]
 
-##            if x>cam_width/2 :
-##                servoX+=1
-##            elif x<cam_width/2:
-##                servoX-=1
-
-##            errorX=cam_width/2-x;
-##            servoX=int(-errorX/25+90)
-
+            #PI controller for X axis
             errorX=(cam_width/2-x);
             integralX+=errorX
             servoX=int(errorX/40+integralX/100+90)
-            
-##            print('errorY='+str(errorY))
-##            print('integralY='+str(integralY))
             print('servoX='+str(servoX))
 
+            #PI controller for the Y axis
             errorY=-(cam_height/2-y);
             integralY+=errorY
             servoY=int(errorY/40+integralY/100+90)
+            print('servoY='+str(servoY))
+                        
+            updateServos(servoX,servoY)
 
+            #check calibration flag and calculating the localization
             if calibratedFlag==1:
                 beta=90-(servoY-deltaAngleY)
                 d=h*np.tan(np.deg2rad(beta))
                 alpha=-(servoX-deltaAngleX)
                 xt=d*np.cos(np.deg2rad(alpha))
                 yt=d*np.sin(np.deg2rad(alpha))
-##                print('beta= '+str(alpha))
-                #print('Xt= '+str(xt)+'Yt= '+str(yt))
                 tmp='Xt:'+str(int(xt))+'   Yt:'+str(int(yt))+'    (cm)'
                 cv2.putText(frame,tmp,(50,100),cv2.FONT_HERSHEY_COMPLEX,
                                     1,(0,255,0),2, cv2.LINE_AA)
                 
-
-            
-            
-##            print('errorY='+str(errorY))
-##            print('integralY='+str(integralY))
-            print('servoY='+str(servoY))
-            
-            updateServos(servoX,servoY)
-##            if x-cam_width>10 :
-##                servoX-=1
-##            elif (x-cam_width<-10):
-##                servoX+=1
-                
-##            if (y-cam_height>10):
-##                servoY-=1
-##            elif (y-cam_height<-10):
-##                servoY+=1
-
-                
-        
-##  # update the points queue
-##  pts.appendleft(center)
-##
-##  # loop over the set of tracked points
-##  for i in range(1, len(pts)):
-##      # if either of the tracked points are None, ignore
-##      # them
-##      if pts[i - 1] is None or pts[i] is None:
-##          continue
-##
-##      # otherwise, compute the thickness of the line and
-##      # draw the connecting lines
-##      thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-##      cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
         
     # show the frame to our screen
@@ -237,34 +195,6 @@ while True:
         deltaAngleY=servoY
         calibratedFlag=1
         print('Calibrated...')
-
-#'h' is the focal length of the camera
-#'X0' is the correction term of shifting of x-axis
-#'Y0' is the correction term ofshifting of y-axis
-#'time0' is the correction term for correction of starting of time
-h = 0.2
-X0 = -3
-Y0 = 20
-time0 = 0
-theta0 = 0.3
-
-#Applying the correction terms to obtain actual experimental data
-Data_Points['x'] = Data_Points['x']- X0
-Data_Points['y'] = Data_Points['y'] - Y0
-Data_Points['time'] = Data_Points['time'] - time0
-
-#Calulataion of theta value
-Data_Points['theta'] = 2 * np.arctan(Data_Points['y']*0.0000762/h)#the factor correspons to pixel length in real life
-Data_Points['theta'] = Data_Points['theta'] - theta0
-
-#Creating the 'Theta' vs 'Time' plot
-plt.plot(Data_Points['theta'], Data_Points['time'])
-plt.xlabel('Theta')
-plt.ylabel('Time')
-
-#Export The Data Points As cvs File and plot
-Data_Points.to_csv('Data_Set.csv', sep=",")
-plt.savefig('Time_vs_Theta_Graph.svg', transparent= True)
 
 # cleanup the camera and close any open windows
 camera.release()
